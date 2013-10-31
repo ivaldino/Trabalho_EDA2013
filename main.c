9a9fe77b8bc0d4804aa7476db44f8f138e45acb5
Eliseu Egewarth <eliseuegewarth@gmail.com>
2013-10-31 01:05:15 -0200

    funcao de cadastro

--- main.c
+++ main.c
@@ -5,15 +5,35 @@
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /* Prototipos*/
 void menu();
-
+Cidade* cadastro();
+int valida_Rendimento(Rendimento r);
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 Cidade* cadastro(){
 	Cidade* c=(Cidade*) malloc(sizeof(Cidade));
 	//printf("\n");
 	printf("Digite o nome da cidade:\n");
-	gets(c->nome);
-	while(c->nome[0]=='\0');
+	do{
+		gets(c->nome);
+	}while(c->nome[0]=='\0');
+	printf("Digite o estado:\n");
+	do{
+		gets(c->estado);
+	}while(c->estado[0]=='\0');
+	printf("Digite o rendimento total da cidade:\n");
+	scanf("%ld", c->rende.total);
+	scanf("%ld", c->rende._1SalMin);
+	scanf("%ld", c->rende._1a2SalMin);
+	scanf("%ld", c->rende._2a3SalMin);
+	scanf("%ld", c->rende._3a5SalMin);
+	scanf("%ld", c->rende._5a10SalMin);
+	scanf("%ld", c->rende._10a20SalMin);
+	scanf("%ld", c->rende._20maisSalMin); 
+	scanf("%ld", c->rende.semRendimento);
+	if((*c).rende.total!=((*c).rende._1SalMin +(*c).rende._1a2SalMin +(*c).rende._2a3SalMin +(*c).rende._3a5SalMin 
+		+(*c).rende._5a10SalMin +(*c).rende._10a20SalMin +(*c).rende._20maisSalMin +(*c).rende.semRendimento)){
+			return NULL;//tranformar calculo em funcao de validacao. protótipo: int valida_Rendimento(Rendimento r);
+	}else return c;
 }
 
 int main(){
